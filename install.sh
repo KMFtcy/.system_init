c54997852a5d1298d087fb0ce81175b344fa573e
root <root@DESKTOP-G61KQFL.localdomain>
2020-10-07 10:49:49 +0800

    add docker

--- install.sh
+++ install.sh
@@ -91,6 +91,24 @@ EOF
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 			# install vim plugins
 			vim -c PlugInstall -c q -c q
+
+			
+	# ===
+	# === docker
+	# ===
+	apt-get install \
+	  apt-transport-https \
+	  ca-certificates \
+		curl \
+		gnupg-agent \
+		software-properties-common
+	curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+	add-apt-repository \
+    "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/ \
+		$(lsb_release -cs) \
+		stable"	
+  apt-get update -y
+	apt-get install -y docker-ce docker-ce-cli containerd.io
 }
 
 
